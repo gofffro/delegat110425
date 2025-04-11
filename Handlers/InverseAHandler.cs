271a170a2f4f9585c31b93436b81251756b68166
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-11 22:31:53 +0700

    Добавил обработчики получения обратных матриц

---
+++ Handlers/InverseAHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace MatrixApp
+{
+  public class InverseAHandler : AbstractMatrixHandler
+  {
+    public override void Handle(Matrix matrixA, Matrix matrixB, Matrix cloneMatrixA, Matrix cloneMatrixB, string choice)
+    {
+      if (choice == "12")
+      {
+        Console.WriteLine("Обратная A:\n" + matrixA.Inverse());
+      }
+      else
+      {
+        base.Handle(matrixA, matrixB, cloneMatrixA, cloneMatrixB, choice);
+      }
+    }
+  }
+}
---
+++ Handlers/InverseBHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace MatrixApp
+{
+  public class InverseBHandler : AbstractMatrixHandler
+  {
+    public override void Handle(Matrix matrixA, Matrix matrixB, Matrix cloneMatrixA, Matrix cloneMatrixB, string choice)
+    {
+      if (choice == "13")
+      {
+        Console.WriteLine("Обратная B:\n" + matrixB.Inverse());
+      }
+      else
+      {
+        base.Handle(matrixA, matrixB, cloneMatrixA, cloneMatrixB, choice);
+      }
+    }
+  }
+}
