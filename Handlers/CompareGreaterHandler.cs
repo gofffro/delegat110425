039f6d2f543fb09c7e731d9b1c38fa77ade7dbe2
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-11 22:31:04 +0700

    Добавил обработчики сравнения матриц (> и <)

---
+++ Handlers/CompareGreaterHandler.cs
@@ -0,0 +1,17 @@
+﻿namespace MatrixApp
+{
+  public class CompareGreaterHandler : AbstractMatrixHandler
+  {
+    public override void Handle(Matrix matrixA, Matrix matrixB, Matrix cloneMatrixA, Matrix cloneMatrixB, string choice)
+    {
+      if (choice == "5")
+      {
+        Console.WriteLine($"A > B: {matrixA > matrixB}");
+      }
+      else
+      {
+        base.Handle(matrixA, matrixB, cloneMatrixA, cloneMatrixB, choice);
+      }
+    }
+  }
+}
