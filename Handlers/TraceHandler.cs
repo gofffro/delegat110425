eee854d5bebcfe411f088cec295e7df9bc95546b
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-11 22:32:11 +0700

    Добавил обработчик вычисления следа матрицы

---
+++ Handlers/TraceHandler.cs
@@ -0,0 +1,18 @@
+﻿namespace MatrixApp
+{
+  public class TraceHandler : AbstractMatrixHandler
+  {
+    public override void Handle(Matrix matrixA, Matrix matrixB, Matrix cloneMatrixA, Matrix cloneMatrixB, string choice)
+    {
+      if (choice == "15")
+      {
+        Console.WriteLine($"След матрицы A = {matrixA.TraceMatrix()}");
+        Console.WriteLine($"След матрицы B = {matrixB.TraceMatrix()}");
+      }
+      else
+      {
+        base.Handle(matrixA, matrixB, cloneMatrixA, cloneMatrixB, choice);
+      }
+    }
+  }
+}
