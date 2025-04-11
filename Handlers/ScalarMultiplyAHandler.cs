3a718bd0e8af82d42f6633ca0d65bad3f97ca91b
Stepan Vazhenin <vazheninstepan@gmail.com>
2025-04-11 22:30:49 +0700

    Добавил обработчики умножения на скаляр (A и B)

---
+++ Handlers/ScalarMultiplyAHandler.cs
@@ -0,0 +1,19 @@
+﻿namespace MatrixApp
+{
+  public class ScalarMultiplyAHandler : AbstractMatrixHandler
+  {
+    public override void Handle(Matrix matrixA, Matrix matrixB, Matrix cloneMatrixA, Matrix cloneMatrixB, string choice)
+    {
+      if (choice == "3")
+      {
+        Console.Write("Введите число - множитель для A: ");
+        double scalarA = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("A * число:\n" + (cloneMatrixA * scalarA));
+      }
+      else
+      {
+        base.Handle(matrixA, matrixB, cloneMatrixA, cloneMatrixB, choice);
+      }
+    }
+  }
+}
